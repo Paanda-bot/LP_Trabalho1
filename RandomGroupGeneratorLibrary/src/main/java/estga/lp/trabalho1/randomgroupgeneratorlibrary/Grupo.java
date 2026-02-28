b068985fd4162a3863f229ec281cbdb0f10ce674
awolf23 <antoniolobo83@gmail.com>
2026-02-28 15:47:15 +0000

    Completa construtor da classe Grupo com validação e normalização da ordem

--- RandomGroupGeneratorLibrary/src/main/java/estga/lp/trabalho1/randomgroupgeneratorlibrary/Grupo.java
+++ RandomGroupGeneratorLibrary/src/main/java/estga/lp/trabalho1/randomgroupgeneratorlibrary/Grupo.java
@@ -6,7 +6,7 @@ public class Grupo {
     private final Estudante e2;
 
     public Grupo(Estudante e1, Estudante e2) {
-        //condição para controlar estudante null
+        //condição para controlo de estudante null
         if (e1 == null || e2 == null) {
             throw new IllegalArgumentException("Grupo inválido: Estudante null.");
         }
@@ -14,6 +14,14 @@ public class Grupo {
         //condição para controlo de estudantes repetidos
         if (e1.getNumero() == e2.getNumero()) {
             throw new IllegalArgumentException("Grupo inválido: Estudantes repetidos");
+        }
+
+        //condição para que a ordem dos grupos não interfira a sua criação: e1/e2 = e2/e1
+
+        if (e2.getNumero() < e1.getNumero()) {
+           Estudante temp = e1;
+           e1 = e2;
+           e2 = temp;
         }
 
         this.e1 = e1;
